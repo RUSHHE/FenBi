c9b7197b9b374954c0115d8d3ac5aa55f51ad921
RUSHHE <rushhe@zjxu.edu.cn>
2024-05-29 12:54:26 +0800

    去除MainActivity所用不到的依赖

--- app/src/main/java/com/example/fenbi/MainActivity.kt
+++ app/src/main/java/com/example/fenbi/MainActivity.kt
@@ -2,13 +2,8 @@ package com.example.fenbi
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
-import com.example.fenbi.dataClass.Question
-import com.example.fenbi.dataClass.QuestionResponseModel
 import com.example.fenbi.databinding.ActivityMainBinding
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : ComponentActivity() {
