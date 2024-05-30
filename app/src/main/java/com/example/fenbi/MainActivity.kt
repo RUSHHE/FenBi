96003d5d70bd4c8c16681bec32005e07d80eb57b
RUSHHE <rushhe@zjxu.edu.cn>
2024-05-30 21:21:47 +0800

    将MainActivity中的内容提取到Fragment中处理

--- app/src/main/java/com/example/fenbi/MainActivity.kt
+++ app/src/main/java/com/example/fenbi/MainActivity.kt
@@ -1,105 +1,63 @@
 package com.example.fenbi
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.fenbi.adapter.ChooseBankAdapter
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.fenbi.databinding.ActivityMainBinding
-import io.reactivex.rxjava3.core.Observer
-import io.reactivex.rxjava3.disposables.Disposable
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var questionMode = ArrayList<Int>().apply {
-        addAll(listOf(0, 0, 20, 1)) // 做题年份, 出题年份（无用）, 题目数量, 题目类型
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initQuestionMode()
-        val chooseBankObserver = object : Observer<Int> {
-            override fun onSubscribe(d: Disposable) {
-                Log.i("chooseBankObserver", "onSubscribe:$d")
-            }
+        binding.homeViewPager.apply {
+            adapter = object : FragmentStateAdapter(this@MainActivity) {
+                override fun getItemCount(): Int = 5
 
-            override fun onError(e: Throwable) {
-                Log.e("chooseBankObserver", "onError: ${e.message}")
+                override fun createFragment(position: Int): Fragment {
+                    return PracticeFragment()
+                }
             }
-
-            override fun onComplete() {
-                PracticeActivity.actionStart(this@MainActivity, questionMode)
-                Log.i("chooseBankObserver", "onComplete")
-            }
-
-            override fun onNext(t: Int) {
-                questionMode[3] = t // 题目类型
-                Log.i("chooseBankObserver", "onNext: $t")
-            }
+            isUserInputEnabled = false // 禁用滑动
         }
-        binding.questionBankListRv.apply {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = ChooseBankAdapter().apply {
-                this.chooseBankObserver = chooseBankObserver
-            }
-        }
-    }
 
-    private fun initQuestionMode() {
-        // 出题模式初始化
-        when (questionMode[0]) {
-            0 -> {
-                binding.customQuestionLayout.examModeBtn.isChecked = true
-            }
+        binding.bottomNavigation.apply {
+            setOnItemSelectedListener {
+                when(it.itemId) {
+                    R.id.practice_fragment -> {
+                        binding.homeViewPager.currentItem = 0
+                        true
+                    }
 
-            1 -> {
-                binding.customQuestionLayout.examModeBtn.isChecked = true
-            }
-        }
-        // 出题年份初始化
-        when (questionMode[1]) {
-            0 -> {
-                binding.customQuestionLayout.questionYearAnyBtn.isChecked = true
-            }
+                    R.id.lesson_fragment -> {
+                        binding.homeViewPager.currentItem = 1
+                        true
+                    }
 
-            1 -> {
-                binding.customQuestionLayout.questionYearFiveBtn.isChecked = true
-            }
+                    R.id.online_fragment -> {
+                        binding.homeViewPager.currentItem = 2
+                        true
+                    }
 
-            2 -> {
-                binding.customQuestionLayout.questionYearTenBtn.isChecked = true
-            }
-        }
-        binding.customQuestionLayout.examModeBtn.setOnClickListener {
-            if (questionMode[0] != 0) {
-                binding.customQuestionLayout.practiceModeBtn.isChecked = false
-                questionMode[0] = 0
-            } else {
-                binding.customQuestionLayout.examModeBtn.isChecked = true
-            }
-        }
-        binding.customQuestionLayout.practiceModeBtn.setOnClickListener {
-            if (questionMode[0] != 1) {
-                binding.customQuestionLayout.examModeBtn.isChecked = false
-                questionMode[0] = 1
-            } else {
-                binding.customQuestionLayout.practiceModeBtn.isChecked = true
-            }
-        }
-        // 初始化出题数量选择器
-        binding.customQuestionLayout.questionCountNp.apply {
-            wrapSelectorWheel = false
-            displayedValues = arrayOf("5", "10", "15", "20", "25", "30", "35", "40")
-            minValue = 0
-            maxValue = displayedValues.size - 1
-            value = (displayedValues.size - 1) / 2
-            setOnValueChangedListener { picker, oldVal, newVal ->
-                questionMode[2] = Integer.parseInt(displayedValues[newVal])
+                    R.id.discover_fragment -> {
+                        binding.homeViewPager.currentItem = 3
+                       true
+                    }
 
+                    R.id.about_me_fragment -> {
+                        binding.homeViewPager.currentItem = 4
+                        true
+                    }
+
+                    else -> {
+                        false
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/example/fenbi/PracticeFragment.kt
@@ -0,0 +1,136 @@
+package com.example.fenbi
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.fenbi.adapter.ChooseBankAdapter
+import com.example.fenbi.databinding.FragmentPracticeBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+
+class PracticeFragment : Fragment() {
+    private var _binding: FragmentPracticeBinding? = null
+    // 只能在onCreateView与onDestroyView之间的生命周期里使用
+    private val binding: FragmentPracticeBinding get() = _binding!!
+    private var questionMode = ArrayList<Int>().apply {
+        addAll(listOf(0, 0, 20, 1)) // 做题年份, 出题年份（无用）, 题目数量, 题目类型
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        _binding = FragmentPracticeBinding.inflate(inflater, container, false)
+
+        initQuestionMode()
+        val chooseBankObserver = object : Observer<Int> {
+            override fun onSubscribe(d: Disposable) {
+                Log.i("chooseBankObserver", "onSubscribe:$d")
+            }
+
+            override fun onError(e: Throwable) {
+                Log.e("chooseBankObserver", "onError: ${e.message}")
+            }
+
+            override fun onComplete() {
+                PracticeActivity.actionStart(context!!, questionMode)
+                Log.i("chooseBankObserver", "onComplete")
+            }
+
+            override fun onNext(t: Int) {
+                questionMode[3] = t // 题目类型
+                Log.i("chooseBankObserver", "onNext: $t")
+            }
+        }
+        binding.questionBankListRv.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = ChooseBankAdapter().apply {
+                this.chooseBankObserver = chooseBankObserver
+            }
+        }
+
+        // 初始化自定义答题区域
+        val bottomSheetBehavior = BottomSheetBehavior.from(binding.customQuestionCard).apply {
+            state = BottomSheetBehavior.STATE_HIDDEN
+            addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(p0: View, p1: Int) {
+                    // TODO: 可以设置背景是否可被点击
+                }
+
+                override fun onSlide(p0: View, p1: Float) {
+                    binding.questionBankListCl.alpha = 0.5f - p1
+                }
+            })
+        }
+
+        binding.customQuestionBtn.setOnClickListener {
+            bottomSheetBehavior.isHideable = false
+            bottomSheetBehavior.isHideable = true
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initQuestionMode() {
+        // 出题模式初始化
+        when (questionMode[0]) {
+            0 -> {
+                binding.customQuestionLayout.examModeBtn.isChecked = true
+            }
+
+            1 -> {
+                binding.customQuestionLayout.examModeBtn.isChecked = true
+            }
+        }
+        // 出题年份初始化
+        when (questionMode[1]) {
+            0 -> {
+                binding.customQuestionLayout.questionYearAnyBtn.isChecked = true
+            }
+
+            1 -> {
+                binding.customQuestionLayout.questionYearFiveBtn.isChecked = true
+            }
+
+            2 -> {
+                binding.customQuestionLayout.questionYearTenBtn.isChecked = true
+            }
+        }
+        binding.customQuestionLayout.examModeBtn.setOnClickListener {
+            if (questionMode[0] != 0) {
+                binding.customQuestionLayout.practiceModeBtn.isChecked = false
+                questionMode[0] = 0
+            } else {
+                binding.customQuestionLayout.examModeBtn.isChecked = true
+            }
+        }
+        binding.customQuestionLayout.practiceModeBtn.setOnClickListener {
+            if (questionMode[0] != 1) {
+                binding.customQuestionLayout.examModeBtn.isChecked = false
+                questionMode[0] = 1
+            } else {
+                binding.customQuestionLayout.practiceModeBtn.isChecked = true
+            }
+        }
+        // 初始化出题数量选择器
+        binding.customQuestionLayout.questionCountNp.apply {
+            wrapSelectorWheel = false
+            displayedValues = arrayOf("5", "10", "15", "20", "25", "30", "35", "40")
+            minValue = 0
+            maxValue = displayedValues.size - 1
+            value = (displayedValues.size - 1) / 2
+            setOnValueChangedListener { picker, oldVal, newVal ->
+                questionMode[2] = Integer.parseInt(displayedValues[newVal])
+
+            }
+        }
+    }
+}
