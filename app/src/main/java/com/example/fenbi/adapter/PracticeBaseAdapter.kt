3489d79be621f30df3dc493afff91aa691408ce8
RUSHHE <rushhe@zjxu.edu.cn>
2024-05-31 21:22:57 +0800

    整理PracticeBaseAdapter代码

--- app/src/main/java/com/example/fenbi/adapter/PracticeBaseAdapter.kt
+++ app/src/main/java/com/example/fenbi/adapter/PracticeBaseAdapter.kt
@@ -19,7 +19,8 @@ open class PracticeBaseAdapter(
     RecyclerView.Adapter<PracticeBaseAdapter.ViewHolder>() {
     var practiceUtils: PracticeUtils? = null
 
-    inner class ViewHolder(binding: ItemQuestionAreaBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(binding: ItemQuestionAreaBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         val typeTextView: TextView = binding.questionTypeTv
         val contentTextView: TextView = binding.questionContentTv
         val optionRecyclerView: RecyclerView = binding.questionOptionRv
@@ -28,9 +29,8 @@ open class PracticeBaseAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding =
             ItemQuestionAreaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        val viewHolder = ViewHolder(binding)
 
-        return viewHolder
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int = questionDataList.size + 1
@@ -40,59 +40,61 @@ open class PracticeBaseAdapter(
             // 答题卡界面隐藏用不到的组件
             holder.typeTextView.isVisible = false
             holder.contentTextView.isVisible = false
-            holder.optionRecyclerView.adapter = answerSheetAdapter
-            // 以免复用item时候错误添加itemDecoration
-            if (holder.optionRecyclerView.itemDecorationCount == 0) {
-                holder.optionRecyclerView.addItemDecoration(AnswerSheetAdapter.ItemDecoration())
-            } else if (holder.optionRecyclerView.itemDecorationCount == 1 && holder.optionRecyclerView.getItemDecorationAt(0) !is AnswerSheetAdapter.ItemDecoration) {
-                holder.optionRecyclerView.removeItemDecorationAt(0)
-                holder.optionRecyclerView.addItemDecoration(AnswerSheetAdapter.ItemDecoration())
-            }
-            val layoutManager = GridLayoutManager(holder.itemView.context, 5)
-            layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
-                override fun getSpanSize(position: Int): Int {
-                    if (position == layoutManager.itemCount - 1) {
-                        return layoutManager.spanCount
+            holder.optionRecyclerView.apply {
+                adapter = answerSheetAdapter
+                layoutManager = GridLayoutManager(holder.itemView.context, 5).apply {
+                    spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                        override fun getSpanSize(position: Int): Int {
+                            if (position == itemCount - 1) {
+                                return spanCount
+                            }
+                            return 1
+                        }
                     }
-                    return 1
+                }
+                // 以免复用item时候错误添加itemDecoration
+                if (itemDecorationCount == 0) {
+                    addItemDecoration(AnswerSheetAdapter.ItemDecoration())
+                } else if (itemDecorationCount == 1 && getItemDecorationAt(0) !is AnswerSheetAdapter.ItemDecoration) {
+                    removeItemDecorationAt(0)
+                    addItemDecoration(AnswerSheetAdapter.ItemDecoration())
                 }
             }
-            holder.optionRecyclerView.layoutManager = layoutManager
         } else {
             // 显示题目类型和题目内容，在复用答题卡的holder中会被隐藏
-            holder.typeTextView.isVisible = true
-            holder.contentTextView.isVisible = true
-            // 根据题目类型设置题目标签
-            when (questionDataList[position].showType) {
-                1 -> {
-                    holder.typeTextView.text = "单选题"
-                }
+            holder.typeTextView.apply {
+                isVisible = true
+                // 根据题目类型设置题目标签
+                text = when (questionDataList[position].showType) {
+                    1 -> "单选题"
 
-                2 -> {
-                    holder.typeTextView.text = "多选题"
-                }
+                    2 -> "多选题"
 
-                3 -> {
-                    holder.typeTextView.text = "判断题"
+                    3 -> "判断题"
+
+                    else -> ""
                 }
             }
-            // 题目内容
-            holder.contentTextView.text = questionDataList[position].content
+            holder.contentTextView.apply {
+                isVisible = true
+                text = questionDataList[position].content // 题目内容
+            }
 
-            val layoutManager = LinearLayoutManager(holder.itemView.context)
-            holder.optionRecyclerView.layoutManager = layoutManager
-            // 子RecyclerView表示选项布局
-            holder.optionRecyclerView.adapter =
-                BaseOptionAdapter(questionDataList[position], userAnswerLists[position]).apply {
-                    this@apply.practiceUtils = this@PracticeBaseAdapter.practiceUtils
-                    answerSheetObserver = answerSheetAdapter.answerSheetObserver
+            holder.optionRecyclerView.apply {
+                layoutManager = LinearLayoutManager(holder.itemView.context)
+                adapter =
+                    BaseOptionAdapter(questionDataList[position], userAnswerLists[position]).apply {
+                        this.practiceUtils = this@PracticeBaseAdapter.practiceUtils
+                        // 子RecyclerView表示选项布局
+                        answerSheetObserver = answerSheetAdapter.answerSheetObserver
+                    }
+                // 以免复用item时候错误添加itemDecoration
+                if (itemDecorationCount == 0) {
+                    addItemDecoration(BaseOptionAdapter.ItemDecoration())
+                } else if (itemDecorationCount == 1 && getItemDecorationAt(0) is AnswerSheetAdapter.ItemDecoration) {
+                    removeItemDecorationAt(0)
+                    addItemDecoration(BaseOptionAdapter.ItemDecoration())
                 }
-            // 以免复用item时候错误添加itemDecoration
-            if (holder.optionRecyclerView.itemDecorationCount == 0) {
-                holder.optionRecyclerView.addItemDecoration(BaseOptionAdapter.ItemDecoration())
-            } else if (holder.optionRecyclerView.itemDecorationCount == 1 && holder.optionRecyclerView.getItemDecorationAt(0) is AnswerSheetAdapter.ItemDecoration) {
-                holder.optionRecyclerView.removeItemDecorationAt(0)
-                holder.optionRecyclerView.addItemDecoration(BaseOptionAdapter.ItemDecoration())
             }
         }
     }
